06dbdb1ec70b5c0507553cec0758d7fb94d02da5
Jasmine Le <jal173@ucsd.edu>
2026-04-23 23:59:03 -0700

    new styling/content

--- global.js
+++ global.js
@@ -123,3 +123,53 @@ form?.addEventListener('submit', function (event) {
   url += params.join('&');
   location.href = url;
 });
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+    
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+//export function renderProjects(project, containerElement) {
+  // Your code will go here
+  //containerElement.innerHTML = '';
+  //const article = document.createElement('article');
+
+  //article.innerHTML = `
+  //  <h3>${project.title}</h3>
+  //  <img src="${project.image}" alt="${project.title}">
+   // <p>${project.description}</p>
+//`;
+//containerElement.appendChild(article);
+
+//}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  // write javascript that will allow dynamic heading levels based on previous function
+  containerElement.innerHTML = '';
+  projects.forEach(project => {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  });
+}
+
+export async function fetchGitHubData(username) {
+  // return statement here
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
